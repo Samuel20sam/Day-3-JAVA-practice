6ee79fcb1ad9b2e81699d7d1e01e46d9e6ca11a4
Samuel_Elijah <samuel20sam@gmail.com>
2022-03-30 17:23:34 +0530

    Refracting: Class EWBI

--- EmpWage/src/com/bridgelabz/empwage/EmpWage.java
+++ EmpWage/src/com/bridgelabz/empwage/EmpWage.java
@@ -27,7 +27,7 @@ public class EmpWage
         this.fullDayHour = fullDayHour;
         this.partTimeHour = partTimeHour;
     }
-    
+
     public void computeEmpWage()
     {
         int empWage = 0;
@@ -41,26 +41,26 @@ public class EmpWage
             Random num = new Random();
             int empCheck = num.nextInt(2)+1;
 
-                if( empCheck == IS_PART_TIME)
-                    if (totalWorkingHours + partTimeHour <= workingHoursPerMonth)
-                    {
-                        empWage = wagePerHour * partTimeHour;
-                        totalWorkingHours = totalWorkingHours + partTimeHour;
-                    }
-                if ( empCheck == IS_FULL_TIME)
-                    if (totalWorkingHours + fullDayHour <= workingHoursPerMonth)
-                        {
-                        empWage = wagePerHour * fullDayHour;
-                        totalWorkingHours = totalWorkingHours + fullDayHour;
-                        }
+            if( empCheck == IS_PART_TIME)
+                if (totalWorkingHours + partTimeHour <= workingHoursPerMonth)
+                {
+                    empWage = wagePerHour * partTimeHour;
+                    totalWorkingHours = totalWorkingHours + partTimeHour;
+                }
+            if ( empCheck == IS_FULL_TIME)
+                if (totalWorkingHours + fullDayHour <= workingHoursPerMonth)
+                {
+                    empWage = wagePerHour * fullDayHour;
+                    totalWorkingHours = totalWorkingHours + fullDayHour;
+                }
             dailyWage.add(empWage);
             totalEmpWage = totalEmpWage + empWage;
             totalWorkingDays++;
-            System.out.println("Total Wage for Day" + (totalWorkingDays) + " is : " + totalEmpWage);
+            System.out.println("Total Wage for Day " + (totalWorkingDays) +" of " +companyName +" is : " + totalEmpWage);
         }
-        System.out.println("Total Working Days :" + totalWorkingDays);
-        System.out.println("Total Working Hours :" + totalWorkingHours);
-        System.out.println("Employee Wage :" + totalEmpWage);
+        System.out.println("Total Working Days " +"of " +companyName +" is : " + totalWorkingDays);
+        System.out.println("Total Working Hours " +"of " +companyName +" is : " + totalWorkingHours);
+        System.out.println("Employee Wage " +"of " +companyName +" is : " + totalEmpWage);
     }
     @Override
     public String toString() {
@@ -78,10 +78,13 @@ public class EmpWage
 
     public static void main(String[] args)
     {
-        EmpWageBuilder.createEmployeeWage();
-        for (Object employeeObject : EmpWageBuilder.array)
+        EmpWageBuilder empWage = new EWBI();
+        empWage.createWageBuilder();
+        for (Object employeeObject : EWBI.array)
         {
             System.out.println(employeeObject);
         }
+        int EmpWage = empWage.getTotalWage("Airtel");
+        System.out.println(EmpWage);
     }
 }
--- EmpWage/src/com/bridgelabz/empwage/EmpWageBuilder.java
+++ EmpWage/src/com/bridgelabz/empwage/EmpWageBuilder.java
@@ -1,21 +1,8 @@
 package com.bridgelabz.empwage;
 
-import java.util.ArrayList;
-
-public class EmpWageBuilder
+public interface EmpWageBuilder
 {
-   static ArrayList <EmpWage> array = new ArrayList();
+    void createWageBuilder();
+    int getTotalWage (String companyName);
 
-        static void createEmployeeWage()
-        {
-            EmpWage paypal = new EmpWage("PayPal",40,25,120,0,8,4);
-            paypal.computeEmpWage();
-            EmpWage tesla = new EmpWage("Tesla",35,25,95,0,8,4);
-            tesla.computeEmpWage();
-            EmpWage airtel = new EmpWage("Airtel",30,25,105,0,8,4);
-            airtel.computeEmpWage();
-            array.add(paypal);
-            array.add(tesla);
-            array.add(airtel);
-        }
 }
